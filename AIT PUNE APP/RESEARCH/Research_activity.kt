83723ebde6cfdf697614e7d503d8a4884b5f578a
Tejaswi Jadhav <94731975+tejaswijadhav2003@users.noreply.github.com>
2022-01-15 16:13:41 +0530

    commit

---
+++ AIT PUNE APP/RESEARCH/Research_activity.kt
@@ -0,0 +1,11 @@
+package com.example.aitpuneapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class research_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_research)
+    }
+}
---
+++ AIT PUNE APP/SCHOLARSHIP/scholarship.kt
@@ -0,0 +1,42 @@
+package com.example.aitpuneapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import android.R
+
+import android.webkit.WebView
+
+
+
+
+
+
+class scholarship : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(com.example.aitpuneapp.R.layout.fragment_scholarship, container, false)
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
